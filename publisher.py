fc47b7b1a5c6a61dfb0eac45216ddf55932cae49
Maxim Schmidt <maxim.schmidt@tum.de>
2021-12-24 20:11:29 +0100

    default reply_to

--- publisher.py
+++ publisher.py
@@ -17,6 +17,8 @@ class BasicPublisher:
 
   def __init__(self, ch):
     self._ch = ch
+    # Default reply_to
+    self.reply_to = None
 
   async def publish(self, message, exchange='', routing_key='', reply_to=None, persistent=False, corr_id=None, timeout=None, headers={}):
     """
@@ -28,20 +30,22 @@ class BasicPublisher:
     :param timeout:
     """
 
-    body = json.dumps(message).encode()
-
     log.debug('Publish message: %s', str(message))
 
-    frame = inspect.currentframe()
-    args, _, _, locals = inspect.getargvalues(frame)
-    props = {arg : locals[arg] for arg in args[2:]}
-    log.debug('Message properties: %s', str(props))
+    body = json.dumps(message).encode()
+
+    reply_to = reply_to or self.reply_to
 
     delivery_mode = 1
     if persistent:
       delivery_mode = 2
 
     timeout = timeout or self.PUBLISH_TIMEOUT
+
+    frame = inspect.currentframe()
+    args, _, _, locals = inspect.getargvalues(frame)
+    props = {arg : locals[arg] for arg in args[2:]}
+    log.debug('Message properties: %s', str(props))
 
     try:
       await self._ch.basic_publish(
@@ -63,16 +67,14 @@ class BasicPublisher:
 class DirectPublisher(BasicPublisher):
   """
   Publish directly to a queue using the default exchange
-
-  Used for bidirectional or unidirectional communication (depends on whether you specify a source)
   """
   
   QUEUE_DECLARE_TIMEOUT = 1
 
-  def __init__(self, ch, dest, source=None):
+  def __init__(self, ch, dest, reply_to=None):
     self._ch = ch
     self._dest = dest
-    self._source = source
+    self.reply_to = reply_to
 
   # TODO: Do you use this? or instead use the resource class BasicQueue directly to avoid redundancies?
   async def init_dest_queue(self, **kwargs):
@@ -81,7 +83,7 @@ class DirectPublisher(BasicPublisher):
 
     :raises: TimeoutError
     """
-    # TODO: passive=true should only **ensure** that it exists (throw an error if not)
+    # TODO: passive=true should only ensure that it exists (throw an error if not)
 
     kwargs['queue'] = self._dest
     if 'timeout' not in kwargs:
@@ -89,22 +91,22 @@ class DirectPublisher(BasicPublisher):
     result = await self._ch.queue_declare(**kwargs)
 
   async def publish(self, message, **kwargs):
-    await super().publish(message, exchange='', routing_key=self._dest, reply_to=self._source, **kwargs)
+    await super().publish(message, exchange='', routing_key=self._dest, **kwargs)
 
 class ExchangePublisher(BasicPublisher):
 
   # TODO: check whether exchange exists? Or assume that?
 
-  def __init__(self, ch, exchange: str, source=None):
+  def __init__(self, ch, exchange: str, reply_to=None):
     self._ch = ch
 
     assert isinstance(exchange, str)
     self._exchange = exchange
 
-    self._source = source
+    self.reply_to = reply_to
 
   async def publish(self, message, **kwargs):
-    await super().publish(message, exchange=self._exchange, reply_to=self._source, **kwargs)
+    await super().publish(message, exchange=self._exchange, **kwargs)
 
 class HeadersExchangePublisher(ExchangePublisher):
 
